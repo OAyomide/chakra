53e940aeba648957aad574a28537f770227acfe3
Ayomide <onigbindeayomide@gmail.com>
2019-01-06 20:11:41 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    println!("Hello, world!");
+    let cl = colorize();
+    println!("Hello:: {}", clgit )
+}
+
+
+fn colorize() -> i32 {
+    let white = 0xffffff;
+    return white
+}
